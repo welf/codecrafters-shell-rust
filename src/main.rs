61dbb51fe86bf407303420562a43f47757239cf5
Arthur Welf <arthur.welf@gmail.com>
2024-05-25 01:37:11 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     loop {
@@ -9,6 +10,9 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+        match input.trim() {
+            "exit 0" => process::exit(0),
+            _ => println!("{}: command not found", input.trim()),
+        }
     }
 }
