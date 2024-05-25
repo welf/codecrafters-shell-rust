313dc1d9880ec1485b443f8d73fff3feab402800
Arthur Welf <arthur.welf@gmail.com>
2024-05-25 04:11:25 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ enum Command {
     Exit,
     Echo,
     Type,
-    Unknown,
+    Unknown(String),
 }
 
 impl From<&str> for Command {
@@ -15,7 +15,7 @@ impl From<&str> for Command {
             "exit" => Command::Exit,
             "echo" => Command::Echo,
             "type" => Command::Type,
-            _ => Command::Unknown,
+            _ => Command::Unknown(s.to_owned()),
         }
     }
 }
@@ -24,29 +24,42 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+
         // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let (command, args) = input.trim().split_once(' ').unwrap_or(("", ""));
+        let mut parts = input.trim().split_whitespace();
 
-        match Command::from(command) {
+        match Command::from(parts.next().unwrap_or_default()) {
             Command::Exit => process::exit(0),
-            Command::Echo => println!("{}", args),
+            Command::Echo => println!("{}", parts.collect::<Vec<&str>>().join(" ")),
             Command::Type => {
-                let mut args = args.trim().split_whitespace();
-                let next_arg = args.next().unwrap();
+                let next_arg = parts.next().unwrap_or_default();
                 match Command::from(next_arg) {
-                    Command::Unknown => {
-                        println!("{} not found", next_arg)
-                    }
+                    Command::Unknown(command) => check_command(command),
                     _ => {
                         println!("{} is a shell builtin", next_arg)
                     }
                 }
             }
-            Command::Unknown => println!("{}: command not found", input.trim()),
+            Command::Unknown(command) => println!("{}: command not found", command),
         }
+    }
+}
+
+fn check_command(command: String) {
+    let path_variable = std::env::var("PATH").unwrap();
+    let paths = path_variable.split(':');
+    let mut path = paths.filter(|path| {
+        let full_command_path = format!("{}/{}", path, command);
+        std::path::Path::new(&full_command_path).exists()
+    });
+    if let Some(p) = path.next() {
+        let path = format!("{}/{}", p, command);
+        println!("{command} is {path}");
+    } else {
+        println!("{command} not found");
     }
 }
